df7e7c7aa679b1036626d2078935963223e91719
shunsaku0621 <hamagi.0621@icloud.com>
2021-05-12 13:06:27 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,9 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    # binding.pry
+    post = Post.create(content: params[:content])
+    # redirect_to action: :index
+    render json:{post: post}
   end
 end
